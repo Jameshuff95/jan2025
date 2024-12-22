326155a5a52b8e1ce3f301be9343bb71204d8e8d
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-22 10:05:31 -0500

    fixrd netlify deployment issue , removed redundant useEffect reference

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Nav, NavLink, Modal, Button } from 'react-bootstrap';
 
 interface ThemeProps {
