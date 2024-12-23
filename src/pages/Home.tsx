ae2c5e4d43b93bc791c022a899fae1333dec4e96
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-23 06:12:19 -0500

    styling changes, card sizes, hero img, nav buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Row, Figure, FigureImage } from 'react-bootstrap';
 import '../App.css';
 import '../index.css';
+import Me from '../assets/me.png';
+
 import Footer from '../Components/Footer';
 
 const Home: React.FC = () => {
@@ -9,18 +11,27 @@ const Home: React.FC = () => {
     <Container>
       <Card>
         <Card.Body>
-          <Card.Title>Home</Card.Title>
+          <Row>
+            <Figure>
+              <FigureImage
+                style={{
+                  width: '100%',
+                  borderRadius: '50%',
+                }}
+                src={Me}
+                alt="A picture of James Huff"
+              />
+            </Figure>
+          </Row>
+          <Card.Title>Hello, my name is James.</Card.Title>
+          <Card.Text style={{ textAlign: 'center' }}>
+            I am a passionate web developer with a background in retail and
+            sales.
+          </Card.Text>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Body>
-          <Card.Title>Home</Card.Title>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Body>
-          <Card.Title>Home</Card.Title>
-        </Card.Body>
+        <Card.Body></Card.Body>
       </Card>
       <Footer />
     </Container>
