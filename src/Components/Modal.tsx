7dbdf57458af5ee594fdc6333e3a7e4993e9f810
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-25 07:05:16 -0500

    modified modal

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -45,11 +45,11 @@ const Menu: React.FC<ModalProps> = ({ show, handleClose }) => {
         </Row>
         <hr />
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body style={{ height: '70vh' }}>
         <Col
           style={{
-            height: '85vh',
-            justifyContent: 'space-between',
+            justifyContent: 'space-around',
+            height: '100%',
           }}
         >
           {links.map((link, index) => (
@@ -63,7 +63,6 @@ const Menu: React.FC<ModalProps> = ({ show, handleClose }) => {
                 alignItems: 'center',
                 textDecoration: 'none',
                 color: 'var(--card-text)',
-                height: '15%',
                 padding: '2% 5%',
                 fontSize: '4rem',
               }}
@@ -71,33 +70,36 @@ const Menu: React.FC<ModalProps> = ({ show, handleClose }) => {
               {link.text}
             </Link>
           ))}
-          <hr />
-          <Row
-            style={{
-              justifyContent: 'flex-end',
-              height: '15vh',
-            }}
-          >
-            <Button
-              variant="danger"
-              onClick={handleClose}
-              style={{
-                background: 'red',
-                color: 'white',
-                cursor: 'pointer',
-                margin: '0 3rem 2rem 0',
-                padding: '2rem .75rem',
-                width: '40%',
-                border: '1px solid var(--card-text)',
-                borderRadius: '10px',
-                fontSize: '3.5rem',
-              }}
-            >
-              Close
-            </Button>
-          </Row>
         </Col>
       </Modal.Body>
+      <hr />
+
+      <Modal.Footer
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          height: '12vh',
+          padding: '0 1rem 0 0',
+        }}
+      >
+        <Button
+          variant="danger"
+          onClick={handleClose}
+          style={{
+            background: 'red',
+            color: 'white',
+            cursor: 'pointer',
+            width: '40%',
+            height: '75%',
+            border: '1px solid var(--card-text)',
+            borderRadius: '10px',
+            fontSize: '3.5rem',
+          }}
+        >
+          Close
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
